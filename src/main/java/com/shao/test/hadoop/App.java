7f2c06852f2122ebd111a46db25fa10f8df5f75d
shao <1070390214@qq.com>
2018-01-09 22:40:00 +0800

    Initial commit

---
+++ src/main/java/com/shao/test/hadoop/App.java
@@ -0,0 +1,13 @@
+package com.shao.test.hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/shao/test/hadoop/hdfs/HDFSApp.java
@@ -0,0 +1,8 @@
+package com.shao.test.hadoop.hdfs;
+
+/**
+ * Hadoop htfs Java api 操作
+ */
+public class HDFSApp {
+
+}
